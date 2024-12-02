a53738c1ff00dd92cd097bcf2f7ea6311addbc30
Ángel José Torres Barros <129797801+T0rrag@users.noreply.github.com>
2024-12-02 17:21:27 +0100

    Update 1.r

--- 1/1.r
+++ 1/1.r
@@ -12,3 +12,20 @@ pairs_and_distances <- data.frame(Pair = 1:length(distSort), V1 = sorted_V1, V2 
 
 total_distance <- sum(distSort)
 
+left_list <- data1$V1
+
+right_list <- data1$V2
+
+right_counts <- table(right_list)
+
+similarity_score <- 0
+
+
+
+for (num in left_list) {
+     if (num %in% names(right_counts)) {
+         similarity_score <- similarity_score + as.numeric(num) * as.numeric(right_counts[as.character(num)])
+     }
+ }
+ 
+similarity_score
